d43100669e9dc947043e82010759e4e4f10ba242
Aidan Kneller <54820937+AidanKneller@users.noreply.github.com>
2022-01-23 13:58:07 -0600

    Removed secret key to make repo public

--- stucu/settings.py
+++ stucu/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-45)=!orf8wrbpr_z@#=6n(p4m$)lp!3+=(hs@si2x$q&y&kf1@'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
